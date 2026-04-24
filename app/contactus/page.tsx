8cc332f63295ce505a54ab0a1f86246f38aef927
AshenFerna29 <ashen2742@gmail.com>
2026-04-24 11:48:29 +0530

    fixed the info and inquiry in the contact us  page

--- app/contactus/page.tsx
+++ app/contactus/page.tsx
@@ -1,9 +1,13 @@
 import HeroSection from "@/components/contactus/Hero";
+import ContactInfoSection from "@/components/contactus/ContactInfoSection";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function ContactPage() {
     return <>
-        <HeroSection />
         <Navbar />
+        <HeroSection />
+        <ContactInfoSection />
+        <Footer />
     </>;
 }
