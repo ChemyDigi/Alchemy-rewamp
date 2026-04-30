02892fdbcc3147d304fab2b862b4cb0fd5910d3c
Naduni Peiris <peirisnaduni13@gmail.com>
2026-04-30 13:25:51 +0530

    hero

---
+++ app/services-dm/page.tsx
@@ -0,0 +1,19 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import Hero from '@/components/services-dm/Front';
+import ScrollFillText from "@/components/services-dm/ScrollFillText";
+export default function Home() {
+    return (
+        <main className="min-h-screen bg-white text-gray">
+
+            <Navbar />
+            <Hero />
+            <ScrollFillText text="We craft stories, not just campaigns. We build connections, not just clicks." />
+
+            <Footer />
+
+
+
+        </main>
+    );
+}
