02fb62d04e45ea28576b7a589c8865933211a598
Geilee Gehara <dvggehara@gmail.com>
2026-05-04 15:28:52 +0530

    hide the alchemy logo in readmore blog post

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,17 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+  
+  // Hide header on blog slug pages
+  if (pathname.startsWith("/blog/") && pathname !== "/blog") {
+    return null;
+  }
+
   return (
     <header className="absolute top-0 left-0 w-full z-50 bg-transparent py-6 px-12 md:px-16 flex items-center pointer-events-none">
       <div className="pointer-events-auto">
