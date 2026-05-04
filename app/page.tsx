8a33f39ecb9fd4a686ae6bab2579a6ddb6b3a64c
Naduni Peiris <peirisnaduni13@gmail.com>
2026-05-04 17:01:37 +0530

    logos

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,11 @@ import Services from "@/components/home/services";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import PanoramicCarousel from "@/components/home/PanoramicCarousel";
+<<<<<<< Updated upstream
 
+=======
+ 
+>>>>>>> Stashed changes
 export default function Home() {
   return (
     <main className="min-h-screen bg-bg text-gray">
@@ -18,7 +22,13 @@ export default function Home() {
       <Hero />
       <AboutIntro />
       <Services/>
+<<<<<<< Updated upstream
       <PanoramicCarousel />
+=======
+      
+      <PanoramicCarousel />
+       
+>>>>>>> Stashed changes
       <Footer />
     </main>
   );
