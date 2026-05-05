ae7f536e8fb7bf1250c551cbf205a90ccb9dea96
Naduni Peiris <peirisnaduni13@gmail.com>
2026-05-05 09:11:39 +0530

    merge fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
 import AboutIntro from "@/components/home/aboutIntro";
-
-// import Navbar from "@/components/Navbar";
-// import Hero from "@/components/Hero";
-// import Features from "@/components/Features";
-// import Footer from "@/components/Footer";
 import Hero from "@/components/home/hero";
 import Services from "@/components/home/services";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import PanoramicCarousel from "@/components/home/PanoramicCarousel";
-<<<<<<< Updated upstream
 
-=======
- 
->>>>>>> Stashed changes
 export default function Home() {
   return (
     <main className="min-h-screen bg-bg text-gray">
@@ -22,13 +13,7 @@ export default function Home() {
       <Hero />
       <AboutIntro />
       <Services/>
-<<<<<<< Updated upstream
       <PanoramicCarousel />
-=======
-      
-      <PanoramicCarousel />
-       
->>>>>>> Stashed changes
       <Footer />
     </main>
   );
