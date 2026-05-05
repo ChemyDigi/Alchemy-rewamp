08c41e039835cdf37853fd7be5303ee7bf35a911
Naduni Peiris <peirisnaduni13@gmail.com>
2026-05-05 11:58:32 +0530

    adjusted files

--- app/services-it/page.tsx
+++ app/services-it/page.tsx
@@ -2,18 +2,17 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Hero from "@/components/services-it/hero";
 import { WaitlistHero } from "@/components/services-it/waitlist-hero";
-import Work from "@/components/services-it/work"
-import SpinCarousel from "@/components/services-it/SpinCarousel";
+ 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-bg text-gray">
+
       <Navbar />
       <Hero/>
-      {/* <WaitlistHero/> */}
-      <SpinCarousel/>
+      <WaitlistHero/>
       <Footer />
-      <Work/>
+       
     </main>
   );
 }
