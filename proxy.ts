928774b16fa508f63d97e49294dde12a5171df0a
AshenFerna29 <ashen2742@gmail.com>
2026-05-13 17:52:14 +0530

    fixed the build error

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const response = NextResponse.next();
   // Forward the pathname so the root layout can conditionally render
   response.headers.set("x-pathname", request.nextUrl.pathname);
