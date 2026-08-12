ca97198a7ec74b45671571567f6cec815f5aa31d
ChanukiSemini <chanukisemini16@gmail.com>
2026-08-12 16:05:21 +0530

    Add the hero image to the event management services in responsive view

--- components/services-event/hero.tsx
+++ components/services-event/hero.tsx
@@ -51,7 +51,7 @@ export default function ServicesSection() {
         </div>
 
         {/* RIGHT IMAGE */}
-        <div className="relative w-full h-[300px] sm:h-[400px] md:h-[500px] lg:h-[650px] -mt-10 sm:-mt-14 md:-mt-18 lg:-mt-24 hidden lg:block">
+        <div className="relative w-full h-[380px] sm:h-[450px] md:h-[500px] lg:h-[650px] -mt-2 sm:-mt-4 md:-mt-10 lg:-mt-24">
           <Image
             src="/images/services-event/walky_talky.png"
             alt="Event Management Device"
